f0cb0784977d7d8203d37f1928f349a36dbea350
황성영 <ghkdtjddud@gmail.com>
2021-03-19 12:22:45 +0900

    init commit

---
+++ src/main/java/com/koroot/KorootApplication.java
@@ -0,0 +1,13 @@
+package com.koroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KorootApplication.class, args);
+    }
+
+}
