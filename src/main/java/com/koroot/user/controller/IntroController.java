580205f3fe93b45fc82b4165367ec685e2e6c8a4
황성영 <ghkdtjddud@gmail.com>
2021-03-20 10:26:29 +0900

    lay out 정의

--- src/main/java/com/koroot/user/controller/IntroController.java
+++ src/main/java/com/koroot/user/controller/IntroController.java
@@ -4,15 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+public class IntroController {
 
-    @GetMapping("/")
-    public String index(){
-        return "redirect:/main";
-    }
-
-    @GetMapping("/main")
+    @GetMapping("/directions")
     public String main(){
-        return "content/main/main";
+        return "content/intro/directions";
     }
 }
