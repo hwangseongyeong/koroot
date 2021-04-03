d28d58b6d75120b073a9fc84101236523740076a
김명균 <sisihanmk@gmail.com>
2021-04-03 23:51:01 +0900

    1.

---
+++ src/main/java/com/koroot/admin/AdminController.java
@@ -0,0 +1,18 @@
+package com.koroot.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    /**
+     * 게사판 - 등록 화면
+     */
+    @GetMapping("/admin/board/post")
+    public String post(){
+        return "content/admin/board/post";
+    }
+
+
+}
