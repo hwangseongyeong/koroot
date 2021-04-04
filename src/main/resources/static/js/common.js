f725bd5f056e755753bf663dfdbd0893ce90f73b
황성영 <ghkdtjddud@gmail.com>
2021-04-04 15:20:09 +0900

    commit-hsy

---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,54 @@
+function dateFormatter(value) {
+    let d = new Date(value)
+    let month = '' + (d.getMonth() + 1);
+    let day = '' + d.getDate()
+    let year = d.getFullYear();
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    return [year, month, day].join('-');
+}
+
+function initTable() {
+    const $table = $('#table');
+    $table.bootstrapTable('destroy').bootstrapTable({
+        locale: "ko-KR",
+        columns: [
+            {
+                title: '번호',
+                field: 'boardId',
+                align: 'center',
+                width: 20,
+                sortable: true
+            }, {
+                title: '제목',
+                field: 'title',
+                align: 'center',
+                width: 100,
+                sortable: true
+            }, {
+                title: '작성자',
+                field: 'createdBy',
+                align: 'center',
+                width: 20,
+                sortable: true
+            }, {
+                title: '등록일',
+                field: 'createdAt',
+                align: 'center',
+                width: 20,
+                formatter: dateFormatter,
+                sortable: true
+            }, {
+                title: '조회수',
+                field: 'hits',
+                align: 'center',
+                width: 20,
+                sortable: true
+            }
+        ]
+    });
+
+    $table.on('click-row.bs.table', function (e, rowData) {
+        console.log(e,rowData);
+    });
+}
