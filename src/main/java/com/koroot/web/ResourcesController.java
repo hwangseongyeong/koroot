311dce4324c4d2cc412a636678ab55f82ca0743d
황성영 <hsy@wemakeprice.com>
2021-04-12 23:34:57 +0900

    commit - hsy

--- src/main/java/com/koroot/web/ResourcesController.java
+++ src/main/java/com/koroot/web/ResourcesController.java
@@ -1,13 +1,20 @@
 package com.koroot.web;
 
+import com.koroot.domain.entity.BoardPost;
+import com.koroot.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * 자료실
  */
+@RequiredArgsConstructor
 @Controller
 public class ResourcesController {
+
+    private final BoardService boardService;
 
     /**
      * 발간 자료
@@ -27,7 +34,10 @@ public class ResourcesController {
      * 해외입양인 커뮤니티
      */
     @GetMapping("/resources/community")
-    public String community(){
-        return "content/resources/community";
+    public String community(Model model){
+        long boardPostId = 3489;
+        BoardPost boardPost = boardService.getBoardPost(boardPostId);
+        model.addAttribute("boardPost", boardPost);
+        return "content/board/contents";
     }
 }
